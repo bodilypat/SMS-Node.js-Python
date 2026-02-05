d47d64ecb27814b5787136d0e7885dfc1ee46b06
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-04 19:25:50 -0800

    Add files via upload

---
+++ backend/src/utils/hash.js
@@ -0,0 +1,12 @@
+//src/utils/hash.js 
+
+import bcrypt from 'bcryptjs';
+
+export const hashPassword = async (password) => {
+    return bcrypt.hash(password, 10);
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword);
+};
+
