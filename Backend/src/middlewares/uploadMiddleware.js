cf0e38b4b4b6a5039d44d21936204163eba1acf0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-04 08:23:46 -0800

    Add files via upload

---
+++ Backend/src/middlewares/uploadMiddleware.js
@@ -0,0 +1,16 @@
+//src/middleware/uploadMiddleware.js 
+
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
+
